610cd41b6d101a38eb1a2507302d8d19ef8f649c
Luis Garreta <lgarreta@gmail.com>
2017-05-18 07:22:39 -0500

    Added new arbolbin.h

--- clases/13-Binary-Trees/code/arbolbin.h
+++ clases/13-Binary-Trees/code/arbolbin.h
@@ -8,8 +8,44 @@ struct nodoBin {
     ArbolBin *der;
 };
 
-
 ArbolBin *crearNodoBin (int raiz);
-void printInorden (ArbolBin *);
+ArbolBin *adicionarBin(ArbolBin* nodoBin, int raiz);
+
+// Dibuja el arbol
 void dibujarBin (ArbolBin *);
+
+// Compute the number of nodes in a tree.
+int longitudBin(ArbolBin* ); 
+// Devuelve 1 o 0 si el elemento esta o no en el arbol
+int buscarBin (ArbolBin*);
+// Imprime la altura del arbol (raiz + altura mayor rama)
+int alturaBin (ArbolBin *);
+// Retorna el valor minimo
+int minValor(ArbolBin* nodoBin); 
+
+// Imprime en inorden (izq-raiz-der)
+void printInorden (ArbolBin *);
+// Imprime por niveles
+void printNiveles (ArbolBin *);
+
+
+/* Cambia la estructura del arbol
+ Si el arbos es:
+       4
+      / \
+     2   5
+    / \
+   1   3
+
+ este cambia a:
+       4
+      / \
+     5   2
+        / \
+       3   1
+*/
+void espejoBin (ArbolBin *);
+
+
+
 #endif
