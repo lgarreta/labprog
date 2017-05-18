b959573a52204ab1bb885223621bf8daa4ef03bf
Luis Garreta <lgarreta@gmail.com>
2017-05-18 14:07:55 -0500

    Modified arbolbin.h to new functions

--- clases/13-Binary-Trees/code/arbolbin.h
+++ clases/13-Binary-Trees/code/arbolbin.h
@@ -11,20 +11,29 @@ struct nodoBin {
 ArbolBin *crearNodoBin (int raiz);
 ArbolBin *adicionarBin(ArbolBin* nodoBin, int raiz);
 
-// Dibuja el arbol
+// Dibuja el arbol. Esta funcion está implementada en "dibujarbin.c"
 void dibujarBin (ArbolBin *);
-
-// Compute the number of nodes in a tree.
-int longitudBin(ArbolBin* ); 
-// Devuelve 1 o 0 si el elemento esta o no en el arbol
-int buscarBin (ArbolBin*);
-// Imprime la altura del arbol (raiz + altura mayor rama)
-int alturaBin (ArbolBin *);
-// Retorna el valor minimo
-int minValor(ArbolBin* nodoBin); 
 
 // Imprime en inorden (izq-raiz-der)
 void printInorden (ArbolBin *);
+
+// Compute the number of nodes in a tree.
+int longitudBin(ArbolBin* ); 
+
+// Devuelve 1 o 0 si el elemento esta o no en el arbol
+int buscarBin (ArbolBin *ab, int elem);
+// Imprime la altura del arbol (raiz + altura mayor rama)
+int alturaBin (ArbolBin *ab);
+
+
+// Compara si dos arboles binarios son iguales. Retorna 0 (falso) o 1 (verdad)
+int igualesBin (ArbolBin *ab1, ArbolBin *ab2);
+
+// Retorna 0 (falso) o 1 (verdad) si el arbol está balanceado
+// Un arbol binario esta balanceado si sus dos hijos están balanceados y
+// la altura de los hijos difiere en máximo 1.
+int balanceadoBin (ArbolBin *ab);
+
 // Imprime por niveles
 void printNiveles (ArbolBin *);
 
